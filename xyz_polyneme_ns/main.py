e76ab45576d6d627c77082ee71871935455c3e39
Donny Winston <donny@polyneme.xyz>
2022-01-19 12:58:35 -0500

    feat: add skos to default context

--- xyz_polyneme_ns/main.py
+++ xyz_polyneme_ns/main.py
@@ -80,11 +80,12 @@ app = FastAPI(
 API_HOST = os.getenv("API_HOST")
 
 DEFAULT_JSONLD_CONTEXT = {
+    "dct": "http://purl.org/dc/terms/",
+    "skos": "http://www.w3.org/2004/02/skos/core#",
     "owl": "http://www.w3.org/2002/07/owl#",
     "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
     "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
     "xsd": "http://www.w3.org/2001/XMLSchema#",
-    "dct": "http://purl.org/dc/terms/",
 }
 
 
